5870e272f8ed0339e95c41f5ca0cdfdb68b5f5ef
Letícia G. Soares <leticiag.soares.dev@gmail.com>
2024-06-04 19:16:19 -0300

    gallery done and some design updates

--- assets/js/navbar.js
+++ assets/js/navbar.js
@@ -1,34 +1,41 @@
 const menuHamburger = document.querySelector('#menuHamburger');
-let isMenuOpen = false;
 const navbar = document.querySelector(".nav-links");
+let isMenuOpen
+
+
 
 const openMenu = () => {
-    isMenuOpen = !isMenuOpen;
     navbar.style.transform = "translateY(0)";
     navbar.style.opacity = "1";
     navbar.style.display = "inherit";
+    console.log('aberto')
 };
 
 const closeMenu = () => {
-    isMenuOpen = !isMenuOpen;
     navbar.style.transform = "translateY(-600px)";
     navbar.style.opacity = "0";
+    console.log('fechado')
 };
 
 const toggleMenu = () => {
     isMenuOpen ? openMenu() : closeMenu();
+    isMenuOpen= !isMenuOpen
 };
 
 const windowResize = () => {
     if (window.innerWidth > 820) {
         isMenuOpen = true;
+        navbar.style.display = 'flex'
     } else {
         isMenuOpen = false;
         navbar.style.display = "none";
+        console.log('a')
     }
     
     toggleMenu()
 }
+
+document.addEventListener("DOMContentLoaded", windowResize())
 
 window.addEventListener("resize", windowResize);
 menuHamburger.addEventListener("click", toggleMenu);
